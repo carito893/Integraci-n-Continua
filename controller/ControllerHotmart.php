394f782c1aaa75d76aa680b95f10093178520686
carito893 <39678128+carito893@users.noreply.github.com>
2018-07-02 18:39:52 -0500

    Update ControllerHotmart.php

--- controller/ControllerHotmart.php
+++ controller/ControllerHotmart.php
@@ -1,7 +1,6 @@
 <?php
 
 
-
     class ControllerHotmart{
 
         private $pdo;
@@ -11,7 +10,6 @@
                  $this->pdo = new PDO('mysql:host=localhost;dbname=camilo24_poli', 'camilo24_poli' , '_Poli@2018');
                 $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  
                 $this->pdo->exec("set names utf8");
-
 
             } catch (Exception $e) {
                 die($e->getMessage());
@@ -38,8 +36,7 @@
                  $fecha = $i->__GET('fecha');
                  $estado = $i->__GET('status');
                  
-                 
-                 
+                                  
             $stm->bindParam(1, $id);
             $stm->bindParam(2, $precio);
             $stm->bindParam(3, $nombre);
@@ -56,6 +53,4 @@
         }
 
 }
-
-
 ?>
